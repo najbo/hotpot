11f86bc3b38603bd4a108b351efee0b9806b0039
najbo <jb@Mobilon.adelboden.local>
2017-11-25 14:28:47 +0100

    Register seulement pour auth. users

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,7 +49,7 @@
                         <!-- Authentication Links -->
                         @guest
                             <li><a href="{{ route('login') }}">Se connecter</a></li>
-                            <li><a href="{{ route('register') }}">S'enregistrer</a></li>
+                            
                         @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
@@ -68,6 +68,7 @@
                                             {{ csrf_field() }}
                                         </form>
                                     </li>
+                                    <li><a href="{{ route('register') }}">Enregistrer un nouveu utilisateur</a></li>
                                 </ul>
                             </li>
                         @endguest
